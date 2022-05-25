ceed4027fbc77622013b975e047fc5aa20e7ad53
Shahzaib-A530 <bsef19a530@pucit.edu.pk>
2022-05-26 00:12:11 +0500

    4th commit

--- switchActivity/app/src/main/java/com/first/switchactivity/MainActivity.java
+++ switchActivity/app/src/main/java/com/first/switchactivity/MainActivity.java
@@ -2,6 +2,7 @@ package com.first.switchactivity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -16,7 +17,8 @@ public class MainActivity extends AppCompatActivity {
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Intent b = new Intent(MainActivity.this,MainActivity2.class);
+                startActivity(b);
             }
         });
     }
--- switchActivity/app/src/main/java/com/first/switchactivity/MainActivity2.java
+++ switchActivity/app/src/main/java/com/first/switchactivity/MainActivity2.java
@@ -2,7 +2,9 @@ package com.first.switchactivity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity2 extends AppCompatActivity {
@@ -11,5 +13,14 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        btn1 = findViewById(R.id.button2);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent a = new Intent(MainActivity2.this, MainActivity.class);
+                startActivity(a);
+            }
+        });
+
     }
 }
